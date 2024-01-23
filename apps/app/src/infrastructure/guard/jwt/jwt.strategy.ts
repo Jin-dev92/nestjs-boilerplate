2825b2bff2f3e0364020d83551b0ce3e876320bc
Jin <kpasd002@gmail.com>
2024-01-23 17:32:03 +0900

    [Feature] jwt authGuard 추가

---
+++ apps/app/src/infrastructure/guard/jwt/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { Payload } from "./jwt.payload";
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      // 헤더 Authentication 에서 Bearer 토큰으로부터 jwt를 추출하겠다는 의미
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET_KEY, // jwt 생성시 비밀키로 사용할 텍스트 (노출 X)
+      ignoreExpiration: false, // jwt 만료를 무시할 것인지 (기본값: false)
+    });
+  }
+
+  async validate(payload: Payload) {
+    return true;
+  }
+  //   const user = payload.sub === "0";
+  //
+  //   if (user) {
+  //     return user; // request.user에 해당 내용을 넣어준다 (Passport 라이브러리가 해줌)
+  //   } else {
+  //     throw new UnauthorizedException("접근 오류");
+  //   }
+  // }
+}
