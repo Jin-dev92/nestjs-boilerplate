7c3036ee24c04cf46f0988c9ff4b0fd224cfa6b9
Jin <kpasd002@gmail.com>
2024-02-05 17:46:13 +0900

    [Feature ]domain >> factory 구현

--- apps/app/src/domain/factory/user.factory.ts
+++ apps/app/src/domain/factory/user.factory.ts
@@ -1,15 +1,13 @@
-import { EventPublisher } from "@nestjs/cqrs";
+import { EventBus } from "@nestjs/cqrs";
 import { PrismaModel, PrismaService } from "@prisma";
 
 export class UserFactory {
   constructor(
     private readonly prismaService: PrismaService,
-    private readonly eventPublisher: EventPublisher,
+    private readonly eventBus: EventBus,
   ) {}
 
   async createUser(option: PrismaModel.UserCreateInput) {
-    return this.eventPublisher.mergeObjectContext(
-      await this.prismaService.user.create({ data: option }),
-    );
+    return this.eventBus.publish();
   }
 }
