d841d37a9c8032525944ec0f8bf5a0e93b33adcc
Jin <kpasd002@gmail.com>
2024-04-22 11:53:55 +0900

    [fix] 코드 누락된 부분 변경

--- apps/app/src/infrastructure/handler/igdb/igdb.module.ts
+++ apps/app/src/infrastructure/handler/igdb/igdb.module.ts
@@ -1,12 +1,21 @@
+import igdbConfig from "../../../constants/igdb.config";
 import { IgdbHttpConfigFactory } from "./factory";
 import { IgdbService } from "./igdb.service";
 import { HttpModule } from "@nestjs/axios";
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
     HttpModule.registerAsync({
+      imports: [
+        HttpModule,
+        ConfigModule.forRoot({
+          load: [igdbConfig],
+        }),
+      ],
       useClass: IgdbHttpConfigFactory,
+      // inject: [HttpService, ConfigService],
     }),
   ],
   providers: [IgdbService],
