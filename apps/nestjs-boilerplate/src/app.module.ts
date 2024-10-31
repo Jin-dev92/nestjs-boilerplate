eba15cc9aa3ab43b07cf3d3a58ff527d1ded9d0a
Jin-devST <kpasd002@gmail.com>
2024-10-31 14:56:02 +0900

    feat. 웹소켓 채팅 구현 ps. 추후에 openAI 연동 예정

--- apps/nestjs-boilerplate/src/app.module.ts
+++ apps/nestjs-boilerplate/src/app.module.ts
@@ -6,7 +6,7 @@ import { UsersModule } from './users/users.module';
 import { ChatsModule } from './chats/chats.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, UsersModule, ChatsModule],
+  imports: [AuthModule, UsersModule, ChatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/nestjs-boilerplate/src/chats/chats.gateway.ts
+++ apps/nestjs-boilerplate/src/chats/chats.gateway.ts
@@ -9,7 +9,7 @@ import dayjs from 'dayjs';
 
 @WebSocketGateway(4100, {
   namespace: 'chats',
-  cors: process.env.REACT_CLIENT_URL,
+  cors: process.env.REACT_CLIENT_URL || 'http://localhost:3000',
 })
 export class ChatsGateway {
   @WebSocketServer() // 웹소켓 서버 의존성 주입
@@ -18,11 +18,11 @@ export class ChatsGateway {
   constructor(private readonly chatsService: ChatsService) {}
 
   handleConnection(client: Socket) {
-    console.log(`Client connected: ${client.id}`);
+    console.log(`Client connected: ${client.id}`); // @todo winston logger로 변경
   }
 
   handleDisconnect(client: Socket) {
-    console.log(`Client disconnected: ${client.id}`);
+    console.log(`Client disconnected: ${client.id}`); // @todo winston logger로 변경
   }
 
   @SubscribeMessage('chatMessage')
@@ -30,7 +30,7 @@ export class ChatsGateway {
     console.log(`Received message from client ${client.id}: ${data.message}`);
 
     this.server.emit('chatMessage', {
-      user: client.id,
+      clientId: client.id,
       message: data.message,
       timestamp: dayjs().format('YYYY-MM-DD HH:mm:ss'),
     });
