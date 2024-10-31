3a52473290f62eff2551c2fe531f3b698ee466f3
Jin-devST <kpasd002@gmail.com>
2024-10-31 14:33:39 +0900

    feat. 웹소켓 채팅 구현 ps. 추후에 openAI 연동 예정

--- apps/nestjs-boilerplate/src/chats/chats.gateway.ts
+++ apps/nestjs-boilerplate/src/chats/chats.gateway.ts
@@ -1,7 +1,37 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { ChatsService } from './chats.service';
+import { Server, Socket } from 'socket.io';
+import dayjs from 'dayjs';
 
-@WebSocketGateway()
+@WebSocketGateway(4100, {
+  namespace: 'chats',
+  cors: process.env.REACT_CLIENT_URL,
+})
 export class ChatsGateway {
   constructor(private readonly chatsService: ChatsService) {}
+  @WebSocketServer()
+  server: Server;
+
+  handleConnection(client: Socket) {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
+  }
+
+  @SubscribeMessage('chatMessage')
+  handleChatMessage(client: Socket, data: { message: string }): void {
+    console.log(`Received message from client ${client.id}: ${data.message}`);
+
+    this.server.emit('chatMessage', {
+      user: client.id,
+      message: data.message,
+      timestamp: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+    });
+  }
 }
